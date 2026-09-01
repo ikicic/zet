dc9ab7402b06e7c4aca0640a53cf5d33cd7fdbf6
Ivica Kicic <ivicakicic@gmail.com>
2026-09-02 00:41:40 +0200

    Use high precision atlas texture coordinates

--- web/src/WebGLMarkerRenderer.ts
+++ web/src/WebGLMarkerRenderer.ts
@@ -47,7 +47,7 @@ layout(location = 5) in vec4 a_color;
 uniform vec2 u_canvasSize;
 uniform vec2 u_atlasSize;
 
-out vec2 v_texCoord;
+out highp vec2 v_texCoord;
 out vec4 v_color;
 
 void main() {
@@ -67,7 +67,7 @@ void main() {
 const FRAGMENT_SHADER = `#version 300 es
 precision mediump float;
 
-in vec2 v_texCoord;
+in highp vec2 v_texCoord;
 in vec4 v_color;
 uniform sampler2D u_atlas;
 out vec4 fragColor;
